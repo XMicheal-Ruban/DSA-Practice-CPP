61aff0d2883ab462f035ef25276431043d0dd993
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-02-12 19:39:46 +0530

    Time: 0 ms (100%), Space: 34.4 MB (41.46%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,36 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-       if(!root) return nullptr;
-       if(key < root->val) root->left = deleteNode(root->left, key);
-       else if(key > root->val) root->right = deleteNode(root->right, key);
-       else {
-        if(!root->left){
-            TreeNode* temp = root->left;
-            delete root;
-            return temp;
+        if (!root) return nullptr;
+
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            // Found the node to delete
+            
+            // Case 1 & 2: Leaf or one child
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            } else if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+            
+            // Case 3: Two children
+            // Find the inorder successor (smallest in the right subtree)
+            TreeNode* temp = root->right;
+            while (temp->left) temp = temp->left;
+            
+            // Swap value
+            root->val = temp->val;
+            
+            // Delete the inorder successor
+            root->right = deleteNode(root->right, temp->val);
         }
-        else if(!root->right){
-            TreeNode* temp = root->left;
-            delete root;
-            return temp;
-        }
-        TreeNode* temp = root->right;
-        while(temp->left) temp = temp->left;
-        root->val = temp -> val;
-        root->right = deleteNode(root->right, temp->val);
-       }
-       return root;
+        return root;
     }
 };
