59542297533e7a27c9db0a0cc7ea0742818412f7
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-02-26 14:55:09 +0530

    Time: 0 ms (100%), Space: 9 MB (30.83%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
     int numSteps(string s) {
-        int num = bitod(s);
-        int steps = 0;
-        while(num != 1){
-            if(num % 2 == 0){
-                steps++;
-                num /= 2;
+        int carry = 0,steps = 0;
+        for(int i = s.length() -1 ; i > 0; i--){
+            if((s[i] & 1) + carry == 1){
+                steps+= 2;
+                carry = 1;
             }
             else{
                 steps++;
-                num++;
             }
         }
-        return steps;
-    }
-    int bitod(string s){
-       int temp = 0, i = 0, j = s.size() -1 , res = 0;
-       while(j > -1){
-        int temp = s[j]-'0';
-        res+= temp*pow(2,i);
-        i++; j--;
-       }
-       return res;
+        return steps + carry;
     }
 };
