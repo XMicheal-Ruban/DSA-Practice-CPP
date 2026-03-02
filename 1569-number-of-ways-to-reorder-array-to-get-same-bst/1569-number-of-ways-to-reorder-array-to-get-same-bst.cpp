bab2a52705461cffae32a11b9751378658ec4ecf
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-03-02 20:07:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
+++ 1569-number-of-ways-to-reorder-array-to-get-same-bst/1569-number-of-ways-to-reorder-array-to-get-same-bst.cpp
@@ -1,29 +1,30 @@
 class Solution {
-    long long mod = 1e9 + 7;
-    vector<vector<long long>> table;
 public:
+long long mod = 1e9 + 7;
+vector<vector<long long>> table;
     int numOfWays(vector<int>& nums) {
         int n = nums.size();
-        table.resize(n + 1, vector<long long>(n + 1, 1));
-        for(int i = 2; i<= n; i++ ){
+        table.resize(n+1);
+        for(int i =0; i <= n; i++){
+            table[i] = vector<long long> (i + 1, 1);
             for(int j = 1; j < i; j++){
                 table[i][j] = (table[i-1][j-1] + table[i-1][j]) % mod;
             }
-        } 
-        return (dfs(nums) - 1 + mod) % mod;
-    }
-    long long dfs(vector<int>& nums ){
-        if(nums.size() <= 2) return 1;
-        vector<int> left, right;
-        int root = nums[0];
-        for(int i = 1; i < nums.size(); i++){
-            if(nums[i] < root) left.push_back(nums[i]);
-            else right.push_back(nums[i]);
         }
-        long long leftways = dfs(left) % mod;
-        long long rightways = dfs(right) % mod;
-        int L = left.size();
-        int R = right.size();
-        return (((table[L + R][L] * leftways) % mod) * rightways) % mod;
+        long long ans = dfs(nums);
+        return ans % mod -1;
+    }
+    long long dfs(vector<int>& nums){
+        int n = nums.size();
+        if(n <= 2) return 1;
+        vector<int> left, right;
+        for(int i = 1; i < n; i++){
+            if(nums[i] > nums[0]) right.push_back(nums[i]);
+            else left.push_back(nums[i]);
+        }
+        long long l_res = dfs(left);
+        long long r_res = dfs(right);
+        int llen = left.size(), rlen = right.size();
+        return ((table[n-1][llen] * l_res)%mod) * r_res % mod;
     }
 };
