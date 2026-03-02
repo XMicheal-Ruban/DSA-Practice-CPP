b2361bcb2b77f0a933669272508c117dc7d2a154
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-03-02 17:45:34 +0530

    Time: 4 ms (26.6%), Space: 9.2 MB (51.03%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -12,12 +12,12 @@ public:
         }
         for(int i = 1; i<=m; i++){
             for(int j = 1; j<= n; j++){
-                if(p[j-1] == s[j-1] || p[j-1] == '.'){
+                if(p[j-1] == s[i-1] || p[j-1] == '.'){
                     dp[i][j] = dp[i-1][j-1];
                 }
                 else if(p[j-1] == '*'){
-                    dp[i][j] = dp[i -1][j-1];
-                    if(p[j-2] == s[j-1] || p[j-2] == '.'){
+                    dp[i][j] = dp[i][j-2];
+                    if(p[j-2] == s[i-1] || p[j-2] == '.'){
                         dp[i][j] = dp[i][j] | dp[i-1][j];
                     }
                 }
