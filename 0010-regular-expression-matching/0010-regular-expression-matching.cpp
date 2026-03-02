b0ddce565e835098704af14f707cdf6342ee7a00
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-03-02 17:44:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,43 +1,28 @@
-#include <iostream>
-#include <vector>
-#include <string>
-
-using namespace std;
-
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        int m = s.length(), n = p.length();
-        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
-        
-        // Base case: empty string and empty pattern match
-        dp[0][0] = true;
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m+1, vector<bool>(n+1, false));
 
-        // Handle patterns like a*, a*b*, a*b*c* matching empty string
-        for (int j = 2; j <= n; j++) {
-            if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 2];
+        dp[0][0] = true;
+        for(int i = 2; i <= n; i++){
+            if(p[i-1] == '*'){
+                dp[0][i] = dp[0][i-2];
             }
         }
-
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (p[j - 1] == s[i - 1] || p[j - 1] == '.') {
-                    // Current characters match
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else if (p[j - 1] == '*') {
-                    // Case 1: 0 occurrences of the character before '*'
-                    dp[i][j] = dp[i][j - 2];
-                    
-                    // Case 2: 1 or more occurrences
-                    // The char before '*' must match current s[i-1]
-                    if (p[j - 2] == s[i - 1] || p[j - 2] == '.') {
-                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<= n; j++){
+                if(p[j-1] == s[j-1] || p[j-1] == '.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1] == '*'){
+                    dp[i][j] = dp[i -1][j-1];
+                    if(p[j-2] == s[j-1] || p[j-2] == '.'){
+                        dp[i][j] = dp[i][j] | dp[i-1][j];
                     }
                 }
             }
         }
-
         return dp[m][n];
     }
 };
