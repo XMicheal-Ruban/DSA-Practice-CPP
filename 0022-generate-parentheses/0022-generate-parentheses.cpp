df982e424c1f9340ede2ef464721553812753fd8
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-03-09 22:04:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        dfs(0 , 0 , "", n, res);
+        dfs(0, 0, "", n, res);
         return res;
     }
-    void dfs(int open , int close, string s, int n, vector<string>& res){
-        if(s.length() == n * 2 ){
+    void dfs(int open, int close, string s, int n, vector<string>& res){
+        if(s.size() == n*2){
             res.push_back(s);
             return;
         }
         if(open < n){
-            dfs(open + 1, close, s + "(", n, res);
+            dfs(open +1, close , s + "(", n , res);
         }
         if(close < open){
-            dfs(open, close + 1, s + ")", n, res);
+            dfs(open, close+1, s + ")", n, res);
         }
     }
 };
