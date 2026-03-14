1a3a37613341b775f5b2128a757b35a510295f12
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-03-14 23:29:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -9,12 +9,19 @@ public:
                 dp[0][i] = dp[0][i-2];
             }
         }
-        for(int i = 0; i<= m; i++){
-            for(int j = 0; j <= n; j++){
-                if(s[j] == p[j] || p[j] == '.'){
-                    dp[i][j] = d[i][j-1];
+        for(int i = 1; i<= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[j-1] == '*'){
+                    dp[i][j] = dp[i][j-2];
+                    if(p[j-2] == s[i-1] || p[j-2] == '.'){
+                        dp[i][j] = dp[i][j] | dp[i-1][j];
+                    }
                 }
             }
         }
+        return dp[m][n];
     }
 };
