111bc65cf0d26b3aeb43d83f72ddf45efd10c45e
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-05-07 18:56:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& pre) {
         vector<int> adj[n];
-        vector<int> indegree(n, 0);
         vector<int> ans;
-        for(auto x: pre){
-            adj[x[0]].push_back(x[1]);
-            indegree[x[1]]++;
+        vector<int> indegree(n , 0);
+        for(auto x : pre){
+            adj[x[1]].push_back(x[0]);
+            indegree[x[0]]++;
         }
         queue<int> q;
         for(int i = 0; i < n; i++){
@@ -18,8 +18,7 @@ public:
             q.pop();
             for(auto x : adj[t]){
                 indegree[x]--;
-                if(indegree[x] == 0)
-                q.push(x);
+                if(indegree[x] == 0) q.push(x);
             }
         }
         return ans.size() == n;
