ba94b315f67ea489248d202b1533d304e1a919bf
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-05-22 20:09:19 +0530

    Time: 6 ms (74.93%), Space: 44.9 MB (10.9%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int  j = 0, i = 0;
+        while(i < g.size() && j < s.size()){
+            if(g[i] <= s[j]) i++;
+            j++;
+        }
+        return i;
+    }
+};
