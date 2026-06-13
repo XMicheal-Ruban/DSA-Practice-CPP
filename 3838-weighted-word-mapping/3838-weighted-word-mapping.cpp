d42367ffaa15f79646f631c3ac3effaf6f69950e
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-06-13 13:39:07 +0530

    Time: 3 ms (27.99%), Space: 43.6 MB (77.6%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans = "";
+        for(int i = 0; i < words.size(); i++){
+            int sum = 0;
+            for(int j = 0; j < words[i].size(); j++){
+                //int idx = (int)words[]
+                sum+= weights[words[i][j] - 'a'];
+            }
+            sum=sum%26;
+            ans+= char( 'z' -sum );
+        }
+        return ans;
+    }
+};
