c728ecde8f3d96dded7f3407c25ebafe80debc2c
XMicheal-Ruban <xmicheal1211@gmail.com>
2026-09-06 18:19:02 +0530

    Time: 942 ms (5%), Space: 252.3 MB (7.33%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        n, m = len(s), len(t)
+        memo = {}
+        def solve(i, j):
+            if j == m:
+                return 1
+            if i == n:
+                return 0
+            if (i , j) in memo:
+                return memo[(i, j)]
+            nottake = solve(i + 1 , j)
+            take = 0
+            if s[i] == t[j]:
+                take = solve (i + 1, j + 1)
+            memo[(i, j)] = take + nottake
+            return memo[(i, j)]
+        return solve(0, 0)
